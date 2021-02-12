7a886bbf856bd32bbcdeb20d2d9181460ebd570f
corbin.boonzaier <corbin.boonzaier@googlemail.com>
2021-02-12 11:23:28 +0200

    updates

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [
+    './mainWebsite/**/*.html',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
